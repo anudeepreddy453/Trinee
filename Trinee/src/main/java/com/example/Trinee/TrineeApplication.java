544184fd9113cd4f5284de1ee57e2b33a04aab5b
anudeepreddy453 <138373989+anudeepreddy453@users.noreply.github.com>
2024-01-04 20:38:18 +0530

    Initial commit

---
+++ Trinee/src/main/java/com/example/Trinee/TrineeApplication.java
@@ -0,0 +1,13 @@
+package com.example.Trinee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrineeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrineeApplication.class, args);
+	}
+
+}
---
+++ Trinee/src/test/java/com/example/Trinee/TrineeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Trinee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrineeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
